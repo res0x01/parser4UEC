fa6a98a8d346d105365a27b85358c61c8798297e
res0x01 <80387370+res0x01@users.noreply.github.com>
2021-10-07 17:45:15 +0900

    changes the output_filename case

--- scrape_landing.py
+++ scrape_landing.py
@@ -4,7 +4,7 @@ from lib.html_to_files import html_to_files
 
 input_filename = "index" # input the fiename for the html file
 
-output_filename = f"{datetime.date.today()}_all"
+output_filename = f"{datetime.date.today()}-all"
 
 if __name__ == "__main__":
     html_to_files(input_filename, output_filename)
